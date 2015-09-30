7d8085c8b60c134cfce4f0984a5c21e8e3dfe5d0
Woojae Kim <ssaskick@gmail.com>
2015-09-30 00:00:35 -0700

    Github commit

--- apps/player.c
+++ apps/player.c
@@ -6,10 +6,7 @@ int main()
 	printf("WEL C T my world\n");
 	foo1();
 	foo2();
-	printf("THIS IS BUG123\n");
-<<<<<<< HEAD
-	printf("hahaha ssss!\n");
-=======
-	prinf("nono this is bug456...\n");
->>>>>>> bug456
+	
+
+	printf("HAHAHA GitHub GOOD!\n");
 }
